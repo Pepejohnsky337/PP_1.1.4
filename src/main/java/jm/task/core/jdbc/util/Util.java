6f620be2a103f18f7bf705de6657a3547661acce
Pepejohnsky337 <pepejohnsky337@vk.com>
2025-12-28 19:35:38 +0400

    исправление с учетом комментариев от ментора 2.0

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Util {
 
     private static final String USER = "root";
-    private static final String PASSWORD = "25Ea084112233";
+    private static final String PASSWORD = "root";
     private static final String URL =
             "jdbc:mysql://127.0.0.1:3306/pepe337db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
